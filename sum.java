e2552e070e2d451c3a45de1a451ab568e21494f1
karthik <karthiknani377@gamill.com>
2026-09-14 20:05:00 +0530

    jira 101

--- sum.java
+++ sum.java
@@ -1,9 +1,15 @@
 public class Main
 {
+       #jira 102
+       void sub()
+         {
+          System.out.println(a-b);
+           }
   public static void mai(String[] args)
    {
      int a=10;
      int b=20;
+
      #jira 103
        void add()
          {
